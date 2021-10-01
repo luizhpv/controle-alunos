f8150ce15ca3e6418ffd549b3860a0fbf2d673b3
luizhpv <luizventura03@gmail.com>
2021-09-30 23:46:21 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const numeroAlunos = 11
+
+console.log('Controle de Alunos');
+for (let cont=0; cont <= numeroAlunos; cont++) {
+    if (numeroAlunos %2 == 0) {
+        if (numeroAlunos = 0) {
+          console.log('0')
+        } else {
+        console.log('par', numeroAlunos);
+        }
+    } else {
+        console.log('ímpar ', numeroAlunos);
+    }
+}
